1d60bbf2352d7083159058c5b8f8c7e46765226c
a22Dv <a22dev.gl@gmail.com>
2025-08-08 21:02:04 +0800

    Has display, but has a ton of bugs. Due for a refactor.

--- include/glb_app.hpp
+++ include/glb_app.hpp
@@ -1,28 +1,66 @@
 #pragma once
 
-#include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/detail/default_ops.hpp>
+#include <climits>
+#include <cstddef>
+#include <glad/glad.h>
 #include <hello_imgui/runner_params.h>
 #include <string>
-#include <cstddef>
+#include <vector>
 
 namespace glb {
-
+constexpr const std::uint64_t imgWidth{1280};
+constexpr const std::uint64_t imgHeight{720};
+constexpr const std::uint64_t imgCh{3};
 namespace mp = boost::multiprecision;
 
+enum class SpatialInterpretation : int { INTERLEAVED, INTERLEAVED_REVERSED, PLANAR, PLANAR_REVERSED, GRAY_CODE, COUNT };
+
+enum class ColorSpaceInterpretation : int { RGB, HSV, YCBCR, COUNT };
+
+constexpr const char *spGetStr(SpatialInterpretation sp) {
+    switch (sp) {
+    case SpatialInterpretation::INTERLEAVED: return "Interleaved";
+    case SpatialInterpretation::INTERLEAVED_REVERSED: return "Reversed Interleaved";
+    case SpatialInterpretation::PLANAR: return "Planar";
+    case SpatialInterpretation::PLANAR_REVERSED: return "Reversed Planar";
+    case SpatialInterpretation::GRAY_CODE: return "Gray Code";
+    default: return "";
+    }
+}
+
+constexpr const char *clrGetStr(ColorSpaceInterpretation clr) {
+    switch (clr) {
+    case ColorSpaceInterpretation::RGB: return "RGB";
+    case ColorSpaceInterpretation::YCBCR: return "YCbCr";
+    case ColorSpaceInterpretation::HSV: return "HSV";
+    default: return "";
+    }
+}
+
+struct TextureData {
+    std::vector<std::uint8_t> texture{};
+    GLuint textureId{};
+    TextureData() : texture(std::vector<std::uint8_t>(imgWidth * imgHeight * imgCh)) {};
+};
+
 struct ApplicationState {
-    const std::uint64_t width{1280};
-    const std::uint64_t height{720};
-    const std::uint64_t bPixel{24}; // 8 bits per channel, RGB.
     const std::uint64_t minSlider{0};
-    const std::uint64_t maxSlider{UINT64_MAX / 2};
-    const mp::cpp_int maxImgIdx{mp::pow(mp::cpp_int{2}, width * height * bPixel) - 1};
+    const std::uint64_t maxCoarseSlider{UINT64_MAX / 2};
+    const std::uint64_t maxJumpIntervalSlider{6'658'301}; // We jump exactly 1x10^6,648,301 at maximum.
+    const mp::cpp_int maxImgIdx{mp::pow(mp::cpp_int{2}, imgWidth *imgHeight *imgCh *CHAR_BIT) - 1};
+    int spInterp{static_cast<int>(SpatialInterpretation::INTERLEAVED)};
+    int clrInterp{static_cast<int>(ColorSpaceInterpretation::RGB)};
     std::size_t totalLimbs{};
     mp::cpp_int imgIdx{};
-    std::uint64_t sliderIdx{};
+    mp::cpp_int jumpIntervalIdx{};
+    std::uint64_t jumpSliderIdx{};
+    std::uint64_t coarseSliderIdx{};
     std::string path{};
-    bool showInputPanel{true};
+    TextureData textureData{};
+    bool showPanels{true};
 };
 
 class Application {
@@ -30,12 +68,15 @@ class Application {
     ApplicationState state{};
     const std::string title{"Gallery of Babel"};
     HelloImGui::RunnerParams rParams{};
+    void updateTexture();
     void postInit();
     void randomGen();
     void controlWindow();
     void additionalControlWindow();
     void update();
     void sliderInterpolate();
+    void idxInterpolate();
+    void beforeExit();
 
   public:
     void run();
--- src/glb_app.cpp
+++ src/glb_app.cpp
@@ -1,9 +1,18 @@
+#include <algorithm>
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
 #include "glb_app.hpp"
 #include "stb_image.h"
+#include <boost/multiprecision/cpp_dec_float.hpp>
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/cpp_int/import_export.hpp>
+#include <boost/multiprecision/detail/default_ops.hpp>
+#include <boost/multiprecision/detail/min_max.hpp>
 #include <climits>
+#include <cmath>
 #include <cstdint>
+#include <filesystem>
 #include <format>
 #include <hello_imgui/app_window_params.h>
 #include <hello_imgui/docking_params.h>
@@ -14,34 +23,41 @@
 #include <hello_imgui/screen_bounds.h>
 #include <imgui.h>
 #include <random>
-#include <filesystem>
+// #include <algorithm>
 
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
 #include <dwmapi.h>
+#include <libloaderapi.h>
+
 #pragma comment(lib, "dwmapi.lib")
 
-
 /*
-    TODO: Fix weird looking icon issues. Finish additional controls, variable speed seeking. Add actual core 
-    display functionality. 
+    TODO:Add actual core display functionality.
 */
+
 namespace glb {
+
 constexpr const int rgbaChannels{4};
 constexpr const int icoSize{32};
+const double log2_10{std::log2f(10)};
+constexpr const std::uint64_t maxB2{22'118'400}; // 1280 * 720 * 8 * 3, number of bits in a 720p image.
 
 namespace {
-    std::filesystem::path getAssetDir() {
-        std::string execDir{};
-        execDir.resize(512);
-        GetModuleFileNameA(NULL, execDir.data(), 512);
-        std::filesystem::path execPath{execDir};
-        execPath.remove_filename();
-        execPath.append("assets\\");
-        return execPath;
-    }
+
+std::filesystem::path getAssetDir() {
+    std::string execDir{};
+    execDir.resize(512);
+    GetModuleFileNameA(NULL, execDir.data(), 512);
+    std::filesystem::path execPath{execDir};
+    execPath.remove_filename();
+    execPath.append("assets\\");
+    return execPath;
 }
+
+} // namespace
+
 void Application::postInit() {
     /*
         This is Windows-specific. Honestly the default white title bar clashes with the application's theme
@@ -51,63 +67,134 @@ void Application::postInit() {
     HWND hwnd{glfwGetWin32Window(window)};
     COLORREF darkColor = 0x00252322;
     DwmSetWindowAttribute(hwnd, DWMWINDOWATTRIBUTE::DWMWA_CAPTION_COLOR, &darkColor, sizeof(darkColor));
-
     std::filesystem::path assetDir{getAssetDir()};
-
     int icoX{}, icoY{}, icoCh{};
-    assetDir.append("gll_icon.png");
-    void* img = stbi_load(assetDir.string().c_str(), &icoX, &icoY, &icoCh, rgbaChannels);
-    GLFWimage icon{icoSize, icoSize, static_cast<unsigned char*>(img)};
+    assetDir.append("gll_32x32.png");
+    void *img = stbi_load(assetDir.string().c_str(), &icoX, &icoY, &icoCh, rgbaChannels);
+    GLFWimage icon{icoSize, icoSize, static_cast<unsigned char *>(img)};
     glfwSetWindowIcon(window, 1, &icon);
     stbi_image_free(img);
+
+    // Texture creation.
+    glGenTextures(1, &state.textureData.textureId);
+    glBindTexture(GL_TEXTURE_2D, state.textureData.textureId);
+
+    // Filtering and wrapping.
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    // Reserve & unbind.
+    glTexImage2D(
+        GL_TEXTURE_2D, 0, GL_RGB, imgWidth, imgHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, state.textureData.texture.data()
+    );
+    glBindTexture(GL_TEXTURE_2D, 0);
 };
+
+void Application::updateTexture() {
+    switch (state.spInterp) {
+    case static_cast<int>(SpatialInterpretation::INTERLEAVED):
+        mp::export_bits(state.imgIdx, state.textureData.texture.begin(), CHAR_BIT);
+        break;
+    case static_cast<int>(SpatialInterpretation::INTERLEAVED_REVERSED):
+        mp::export_bits(state.imgIdx, state.textureData.texture.begin(), CHAR_BIT);
+        std::reverse(state.textureData.texture.begin(), state.textureData.texture.end());
+    case static_cast<int>(SpatialInterpretation::PLANAR):
+        mp::export_bits(state.imgIdx, state.textureData.texture.begin(), CHAR_BIT);
+        break;
+    case static_cast<int>(SpatialInterpretation::GRAY_CODE): // Broken. 
+        state.imgIdx ^= (state.imgIdx >> 1);
+        mp::export_bits(state.imgIdx, state.textureData.texture.begin(), CHAR_BIT);
+        break;
+    }
+}
 
 void Application::update() {
     if (ImGui::IsKeyPressed(ImGuiKey_H, false)) {
-        state.showInputPanel = !state.showInputPanel;
+        state.showPanels = !state.showPanels;
         for (HelloImGui::DockableWindow &window : HelloImGui::GetRunnerParams()->dockingParams.dockableWindows) {
-            window.isVisible = state.showInputPanel;
+            window.isVisible = state.showPanels;
         }
     }
+    updateTexture();
+    glBindTexture(GL_TEXTURE_2D, state.textureData.textureId);
+    glTexSubImage2D(
+        GL_TEXTURE_2D, 0, 0, 0, imgWidth, imgHeight, GL_RGB, GL_UNSIGNED_BYTE, state.textureData.texture.data()
+    );
+    ImDrawList *bgDrawList{ImGui::GetBackgroundDrawList(ImGui::GetMainViewport())};
+    bgDrawList->AddImage(static_cast<ImTextureID>(state.textureData.textureId), ImVec2{0, 0}, ImVec2{1280, 720});
 }
+
+void Application::beforeExit() {
+    if (state.textureData.textureId) {
+        glDeleteTextures(1, &state.textureData.textureId);
+        state.textureData.textureId = 0;
+    }
+}
+
 void Application::additionalControlWindow() {
-    if (!state.showInputPanel) {
+    if (!state.showPanels) {
         return;
     }
-    ImGui::SliderScalar(
-        "##", ImGuiDataType_::ImGuiDataType_U64, &state.sliderIdx, &state.minSlider, &state.maxSlider, ""
+    ImGui::PushItemWidth(-1);
+    if (ImGui::SliderScalar(
+            "##", ImGuiDataType_::ImGuiDataType_U64, &state.jumpSliderIdx, &state.minSlider,
+            &state.maxJumpIntervalSlider, std::format("Approx. Interval: 1x10^{}", state.jumpSliderIdx).c_str()
+        )) {
+        state.jumpIntervalIdx = mp::pow(mp::cpp_int{10}, state.jumpSliderIdx);
+    }
+    ImGui::SliderInt(
+        "##x", &state.spInterp, 0, static_cast<int>(SpatialInterpretation::COUNT) - 1,
+        spGetStr(static_cast<SpatialInterpretation>(state.spInterp))
     );
+    ImGui::SliderInt(
+        "##xx", &state.clrInterp, 0, static_cast<int>(ColorSpaceInterpretation::COUNT) - 1,
+        clrGetStr(static_cast<ColorSpaceInterpretation>(state.clrInterp))
+    );
+    ImGui::PopItemWidth();
 }
 
 void Application::controlWindow() {
-    if (!state.showInputPanel) {
-        return;
-    }
     /*
        Got these digits from Wolfram-Alpha's online calculator. It's probably correct.
        From 7.1795000302083x10^6,658,301. The maximum number of all 720p RGB images.
    */
     constexpr const double maxMantissa{7.1795000302083};
-    const double mantissa{maxMantissa * (static_cast<double>(state.sliderIdx) / state.maxSlider)};
+    const double mantissa{maxMantissa * (static_cast<double>(state.coarseSliderIdx) / state.maxCoarseSlider)};
 
-    const std::string labelText{std::format("Image no. {}... x 10^6,658,301", mantissa)};
+    const std::string labelText{std::format("Image #{}... x 10^6,658,301", mantissa)};
     const std::string buttonText{"I'm Feeling Lucky!"};
 
     float buttonX{ImGui::CalcTextSize(buttonText.c_str()).x};
     float imgNumX{ImGui::CalcTextSize(labelText.c_str()).x};
     float spacing{ImGui::GetContentRegionMax().x - (buttonX + imgNumX) - 16.0f};
-    ImGui::Text("%s", labelText.c_str());
-    ImGui::SameLine(0.0f, spacing);
-    if (ImGui::Button(buttonText.c_str())) {
+    if (ImGui::Button(buttonText.c_str(), ImVec2{-1, 0})) {
         randomGen();
+        idxInterpolate();
     }
     ImGui::PushItemWidth(-1);
     if (ImGui::SliderScalar(
-            "##", ImGuiDataType_::ImGuiDataType_U64, &state.sliderIdx, &state.minSlider, &state.maxSlider, ""
+            "##", ImGuiDataType_::ImGuiDataType_U64, &state.coarseSliderIdx, &state.minSlider, &state.maxCoarseSlider,
+            labelText.c_str()
         )) {
-        sliderInterpolate();
+        randomGen();
+        state.imgIdx >>= 64;
+        state.imgIdx |= mp::cpp_int{state.coarseSliderIdx}
+                        << ((mp::msb(state.maxImgIdx) + 1) - (sizeof(std::uint64_t) * CHAR_BIT));
     }
     ImGui::PopItemWidth();
+    float availableWidth{ImGui::GetContentRegionAvail().x};
+    float intervalButtonWidth{availableWidth / 2 - 5.0f};
+    if (ImGui::Button("<<", ImVec2{intervalButtonWidth, 0}) || ImGui::IsKeyPressed(ImGuiKey_LeftArrow, true)) {
+        state.imgIdx = mp::max(mp::cpp_int{0}, state.imgIdx - state.jumpIntervalIdx);
+        idxInterpolate();
+    }
+    ImGui::SameLine();
+    if (ImGui::Button(">>", ImVec2{intervalButtonWidth, 0}) || ImGui::IsKeyPressed(ImGuiKey_RightArrow, true)) {
+        state.imgIdx = mp::min(state.maxImgIdx, state.imgIdx + state.jumpIntervalIdx);
+        idxInterpolate();
+    }
 }
 
 Application::Application() {
@@ -115,11 +202,13 @@ Application::Application() {
     rParams.imGuiWindowParams.defaultImGuiWindowType = HelloImGui::DefaultImGuiWindowType::ProvideFullScreenDockSpace;
     rParams.callbacks.PostInit = [this] { postInit(); };
     rParams.dockingParams.dockingSplits.push_back(
-        HelloImGui::DockingSplit{"MainDockSpace", "MainBottom", ImGuiDir_Down, 0.12f}
+        HelloImGui::DockingSplit{"MainDockSpace", "MainBottom", ImGuiDir_Down, 0.14f}
     );
     rParams.dockingParams.dockingSplits.push_back(
         HelloImGui::DockingSplit{"MainBottom", "MainBottomRight", ImGuiDir_Right, 0.5f}
     );
+    rParams.imGuiWindowParams.tweakedTheme.Theme = ImGuiTheme::ImGuiTheme_BlackIsBlack;
+    rParams.imGuiWindowParams.backgroundColor = ImVec4{1.0f, 1.0f, 1.0f, 1.0f};
 
     HelloImGui::DockableWindow controlWindow{"Controls", "MainBottom", [this] { this->controlWindow(); }, true, false};
     HelloImGui::DockableWindow additionalCWindow{
@@ -157,12 +246,6 @@ void Application::randomGen() {
             break;
         }
     }
-    /*
-        Might be fragile. Just take note for possible errors.
-        >> 1 is required due to the fact that ImGui uses doubles internally
-        and cannot represent all of uint64_t in full precision.
-    */
-    state.sliderIdx = ((state.imgIdx >> (totalBits - uint64Sz))).convert_to<std::uint64_t>() >> 1;
 }
 
 void Application::sliderInterpolate() {
@@ -172,6 +255,18 @@ void Application::sliderInterpolate() {
           We have to interpolate this in such a roundabout way because it freezes when you use
           multiplication and division on such a huge number. We only interpolate the top 64 bits of the number.
       */
-    state.imgIdx = mp::cpp_int{state.sliderIdx << 1} << totalBits - uint64Sz;
+    state.imgIdx = mp::cpp_int{state.coarseSliderIdx << 1} << totalBits - uint64Sz;
 }
+
+void Application::idxInterpolate() {
+    constexpr const std::size_t uint64Sz{sizeof(std::uint64_t) * CHAR_BIT};
+    const std::size_t totalBits{mp::msb(state.maxImgIdx) + 1};
+    /*
+        Might be fragile. Just take note for possible errors.
+        >> 1 is required due to the fact that ImGui uses doubles internally
+        and cannot represent all of uint64_t in full precision.
+    */
+    state.coarseSliderIdx = ((state.imgIdx >> (totalBits - uint64Sz))).convert_to<std::uint64_t>() >> 1;
+}
+
 } // namespace glb
